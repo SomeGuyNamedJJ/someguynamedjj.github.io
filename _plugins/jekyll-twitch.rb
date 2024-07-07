d5defda483d6e1db1e17339b92955ab8b9f850c1
SomeGuyNamedJJ <someguynamedjjyt@gmail.com>
2024-07-07 11:34:50 +0100

    Add Twitch Plugin

---
+++ _plugins/jekyll-twitch.rb
@@ -0,0 +1,10 @@
+ frozen_string_literal: true
+
+require 'jekyll-twitch/version'
+require 'jekyll-twitch/twitch_tag'
+
+module Jekyll
+  # This is the module for Twitch stuff for Jekyll
+  module Twitch
+  end
+end
